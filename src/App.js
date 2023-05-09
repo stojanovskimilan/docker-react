ee0be3526c4e8b47be24bcb59431232de962ac48
stojanovskimilan <stojanovski.m@proton.me>
2023-05-09 10:56:00 +0200

    Modification in dockerfiles

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was changed!
+          Hi, Milan!
         </p>
         <a
           className="App-link"
